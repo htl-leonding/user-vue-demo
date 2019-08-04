ac864130d1915357bb827a1486825fdd50e08ade
htl-leonding <t.stuetz@htl-leonding.ac.at>
2019-08-04 11:52:08 +0200

    initial commit

---
+++ src/components/User.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm an awesome User!</p>
+    <button @click="changeName">Change my Name</button>
+    <p>Name is {{ name }}</p>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <app-user-detail
+                :myName="name"
+                @nameWasReset="name = $event"
+                :resetFn="resetName">
+        </app-user-detail>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <app-user-edit></app-user-edit>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import UserDetail from './UserDetail.vue';
+  import UserEdit from './UserEdit.vue';
+
+  export default {
+    data: function () {
+      return {
+        name: 'Hans'
+      }
+    },
+    methods: {
+      changeName() {
+        this.name = "Susi";
+      },
+      resetName() {
+        this.name = 'Hans';
+      }
+    },
+    components: {
+      appUserDetail: UserDetail,
+      appUserEdit: UserEdit
+    }
+  }
+</script>
+
+<style>
+  div {
+    background-color: lightblue;
+  }
+</style>
+
+
